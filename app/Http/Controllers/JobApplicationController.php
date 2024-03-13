5cad61307129dff60086ae799164ec29568d9cd0
natalia.belzarova <natalia.belzarova@gmail.com>
2024-03-13 14:39:39 +0100

    create JobPolicy and hasUserApplied method in Job model

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -9,7 +9,9 @@ class JobApplicationController extends Controller
 {
     
     public function create(Job $job)
+
     {
+        $this->authorize('apply', $job);
         return view('job_application.create', ['job'=>$job]);
     }
 
